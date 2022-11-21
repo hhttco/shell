1ec35fecad25df99ecca869ba9517f236f7fd1fb
hhttco <94968015+hhttco@users.noreply.github.com>
2022-11-21 11:44:12 +0800

    Add files via upload

--- 002.sh
+++ 002.sh
@@ -13,8 +13,8 @@ if [[ $# != 4 ]]; then
     echo -e "${red}错误: 缺少必要参数！${plain}"
     echo -e "${yellow}1.主机地址${plain}"
     echo -e "${yellow}2.token${plain}"
-    echo -e "${yellow}3.节点IDS${plain}"
-    echo -e "${yellow}4.节点类型 0.VMess 1.ShadowSocks 2.Trojan ${plain}"
+    echo -e "${yellow}3.节点IDS 多个英文逗号分隔 ${plain}"
+    echo -e "${yellow}4.节点类型 和IDS一一对应 0.VMess 1.ShadowSocks 2.Trojan ${plain}"
     exit 1
 fi
 
